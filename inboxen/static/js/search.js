c6dd3c9d72b7be7af83d5c55d8f9d0012c5f8639
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-04-14 09:30:51 +0100

    Pluginify search

--- inboxen/static/js/search.js
+++ inboxen/static/js/search.js
@@ -5,48 +5,59 @@
 
 (function($) {
     'use strict';
-    var timer;
-
-    function areWeReadyYet($refreshNote, $searchInfo, timer) {
-        var http = new XMLHttpRequest();
-        http.open("HEAD", $refreshNote.data("url"), true);
-        http.onload = function (e) {
-            if (http.readyState > 2) {
-                if (http.status == 202) {
-                    // not done
-                    $refreshNote.html("");
-                } else if (http.status == 201) {
-                    // done!
-                    clearInterval(timer);
-                    $refreshNote.html("Loading results…");
-                    location.reload(true);
-                } else if (http.status == 400) {
-                    clearInterval(timer);
-                    $searchInfo.html("The search timed out. Please try again.");
-                    $searchInfo.addClass("alert alert-warning");
-                    console.error("Server says there is no such search");
-                } else {
-                    clearInterval(timer);
-                    $searchInfo.html("Something went wrong while searching. Please try again later.");
-                    $searchInfo.addClass("alert alert-warning");
-                    console.error("Unexpected response code");
+    $.fn.inboxenSearch = function() {
+        function areWeReadyYet($refreshNote, $searchInfo, timer) {
+            var http = new XMLHttpRequest();
+            http.open("HEAD", $refreshNote.data("url"), true);
+            http.onload = function (e) {
+                if (http.readyState > 2) {
+                    if (http.status == 202) {
+                        // not done
+                        $refreshNote.html("");
+                    } else if (http.status == 201) {
+                        // done!
+                        clearInterval(timer);
+                        $refreshNote.html("Loading results…");
+                        location.reload(true);
+                    } else if (http.status == 400) {
+                        clearInterval(timer);
+                        $searchInfo.html("The search timed out. Please try again.");
+                        $searchInfo.addClass("alert alert-warning");
+                        console.error("Server says there is no such search");
+                    } else {
+                        clearInterval(timer);
+                        $searchInfo.html("Something went wrong while searching. Please try again later.");
+                        $searchInfo.addClass("alert alert-warning");
+                        console.error("Unexpected response code");
+                    }
                 }
-            }
-        };
-        http.send(null);
-    }
-    var $refreshNote = $("#search-refreshnote");
-    var $searchInfo = $("#search-info");
+            };
+            http.send(null);
+        }
 
-    $("#inboxen-search-box").on("submit", function() {
+        return this.each(function() {
+            var timer;
+            var $this = $(this);
+            var $refreshNote = $this.find("#search-refreshnote");
+            var $searchInfo = $this.find("#search-info");
+
+            if ($refreshNote.length === 0) {
+                return;
+            }
+            $refreshNote.html("");
+            // poll the server every 7000 ms
+            timer = setInterval(function(){areWeReadyYet($refreshNote, $searchInfo, timer);}, 7000);
+        });
+    };
+})(jQuery);
+
+(function($) {
+    'use strict';
+
+    $("#inboxen-search-box").on("submit", function(e) {
         window.location = this.action + this.q.value + "/";
-        return false;
+        e.preventDefault();
     });
 
-    if ($refreshNote.length === 0) {
-        return;
-    }
-    $refreshNote.html("");
-    // poll the server every 7000 ms
-    timer = setInterval(function(){areWeReadyYet($refreshNote, $searchInfo, timer);}, 7000);
+    $(document).inboxenSearch();
 })(jQuery);
