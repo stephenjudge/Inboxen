ed26da077098b9b630f2c8b609272775e2f1b02f
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-04-17 22:29:28 +0100

    Pluginify inbox toggle

--- inboxen/static/js/inbox.js
+++ inboxen/static/js/inbox.js
@@ -30,59 +30,67 @@
         $row.remove();
     }
 
-    $("#email-list button[type=submit]").click(function(event) {
-        event.preventDefault();
+    $.fn.inboxenPinnedBotton = function() {
+        return this.each(function(e) {
+            e.preventDefault();
 
-        var $this = $(this);
-        if ($this.data("clicked") === "yes") {
-            return false;
-        }
-
-        $this.inboxenSpinnerToggle();
-
-        var button = {"name": $this.attr("name"), "value": $this.attr("value")};
-        var form_data = $("#email-list").serializeArray();
-        form_data.push(button);
-
-        $.ajax({
-            type: "POST",
-            url: $("#email-list").data("url"),
-            data: form_data,
-            complete: function(xhr, statusText) {
-                if (xhr.status === 204) {
-                    var $row = $("#email-" + button.value);
-                    // was the important toggle pressed?
-                    if (button.name === "important-single") {
-                        toggleImportant($row);
-                    } else {
-                        // multiple emails were selected
-                        var fn;
-                        if (button.name === "important") {
-                            fn = markImportant;
-                        } else if (button.name === "unimportant") {
-                            fn = unmarkImportant;
-                        } else if (button.name === "delete") {
-                            fn = deleteRow;
-                        } else {
-                            // return early, I don't know what button was pressed
-                            return;
-                        }
-
-                        $.each(form_data, function(index, value) {
-                            if (value.value === "email") {
-                                fn($("#email-" + value.name));
-                            }
-                        });
-                    }
-                } else {
-                    var $messageBlock = $("#alertmessages");
-                    var message = '<div class="alert alert-warning" role="alert">Something went wrong!<button type="button" class="close" data-dismiss="alert"><span class="fa fa-times" aria-hidden="true"></span><span class="sr-only">Close</span></button></div>';
-                    $messageBlock.append(message);
-                }
-
-                // finally, re-enable button
-                $this.inboxenSpinnerToggle();
+            var $this = $(this);
+            if ($this.data("clicked") === "yes") {
+                return;
             }
+
+            $this.inboxenSpinnerToggle();
+
+            var button = {"name": $this.attr("name"), "value": $this.attr("value")};
+            var form_data = $("#email-list").serializeArray();
+            form_data.push(button);
+
+            $.ajax({
+                type: "POST",
+                url: $("#email-list").data("url"),
+                data: form_data,
+                complete: function(xhr, statusText) {
+                    if (xhr.status === 204) {
+                        var $row = $("#email-" + button.value);
+                        // was the important toggle pressed?
+                        if (button.name === "important-single") {
+                            toggleImportant($row);
+                        } else {
+                            // multiple emails were selected
+                            var fn;
+                            if (button.name === "important") {
+                                fn = markImportant;
+                            } else if (button.name === "unimportant") {
+                                fn = unmarkImportant;
+                            } else if (button.name === "delete") {
+                                fn = deleteRow;
+                            } else {
+                                // return early, I don't know what button was pressed
+                                return;
+                            }
+
+                            $.each(form_data, function(index, value) {
+                                if (value.value === "email") {
+                                    fn($("#email-" + value.name));
+                                }
+                            });
+                        }
+                    } else {
+                        var $messageBlock = $("#alertmessages");
+                        var message = '<div class="alert alert-warning" role="alert">Something went wrong!<button type="button" class="close" data-dismiss="alert"><span class="fa fa-times" aria-hidden="true"></span><span class="sr-only">Close</span></button></div>';
+                        $messageBlock.append(message);
+                    }
+
+                    // finally, re-enable button
+                    $this.inboxenSpinnerToggle();
+                }
+            });
         });
-    });
+    };
+})(jQuery);
+
+(function($) {
+    'use strict';
+
+    $("#email-list button[type=submit]").inboxenPinnedBotton();
 })(jQuery);
